2ad26547e09e8c0de70b1042a26b439568951148
differui <differui@gmail.com>
2018-04-19 13:40:49 +0800

    pkg: install typescript compiler

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,22 @@
-const commonjs = require('rollup-plugin-commonjs')
-const replace = require('rollup-plugin-replace')
-const resolve = require('rollup-plugin-node-resolve')
-const typescript = require('rollup-plugin-typescript')
+const commonjs = require('rollup-plugin-commonjs');
+const replace = require('rollup-plugin-replace');
+const resolve = require('rollup-plugin-node-resolve');
+const typescript = require('rollup-plugin-typescript');
 
 export default {
   entry: './src/index.ts',
   dest: './dest/bundle.js',
   format: 'cjs',
   plugins: [
-    typescript(),
+    typescript({
+      typescript: require('typescript'),
+    }),
     commonjs({
       namedExports: {}
     }),
     resolve(),
     replace({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    })
-  ]
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+  ],
 }
